0418a3a8e51f9b9a8786f151b9344b0dfe3cd870
BenoDavid <beno.david@classicfashion.com>
2025-10-15 09:02:33 +0300

    red tag work

--- controllers/AURedTagAuditSchedulesController.js
+++ controllers/AURedTagAuditSchedulesController.js
@@ -9,7 +9,64 @@ class AURedTagAuditSchedulesController extends BaseController {
   constructor() {
     super(AURedTagAuditSchedule);
   }
+  async getById(req, res) {
+    try {
+      const item = await this.model.findOne({
+        where: { id: req.params.id },
+        attributes: ["id", "title", "scheduledDate", "status", "scheduledBy", "createdBy"],
+        include: [
+          {
+            model: this.model.associations.audits.target,
+            as: 'audits',
+            include: [{
+              model: this.model.associations.audits.target?.associations.location.target,
+              as: 'location',
+              attributes: ["name"],
+            }]
+          },
+          {
+            model: this.model.associations.facility.target,
+            as: 'facility',
+            attributes: ["id", "name"],
+            include: [{
+              model: this.model.associations.facility.target?.associations.shifts.target,
+              as: 'shifts',
+              attributes: ["id", "name"],
+            }]
+          },
+          {
+            model: this.model.associations.department.target,
+            as: 'department',
+            attributes: ["id", "name"],
+            include: [{
+              model: this.model.associations.department.target?.associations.locations.target,
+              as: 'locations',
+              attributes: ["id", "name", "questionGroupId"],
+              include: [{
+                model: this.model.associations.department.target?.associations.locations.target?.associations.subLocations?.target,
+                as: 'subLocations',
+                attributes: ["id", "name"],
+              }]
+            }]
+          },
+        ],
+      });
 
+      if (item) {
+        res.status(200).json({
+          status: 200,
+          message: `${this.model.name}s fetched successfully`,
+          result: item,
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        status: 500,
+        message: error.message,
+        result: {},
+      });
+    }
+  }
 }
 
 module.exports = new AURedTagAuditSchedulesController();
--- controllers/AURedTagAuditsController.js
+++ controllers/AURedTagAuditsController.js
@@ -9,7 +9,83 @@ class AURedTagAuditsController extends BaseController {
   constructor() {
     super(AURedTagAudit);
   }
+  async getById(req, res) {
+    try {
+      const item = await this.model.findOne({
+        where: { id: req.params.id },
+        attributes: ["auditor", "createdAt", "safety", "sort", "setInOrder", "shine", "standardize", "sustain", "netScore", "auditor", "status"],
+        include: [
+          {
+            model: this.model.associations.schedule.target,
+            as: 'schedule',
+            attributes: ["finalResult"],
+            include: [
+              {
+                model: this.model.associations.schedule.target?.associations?.facility.target,
+                as: 'facility',
+                attributes: ["name"],
+              }
+            ],
+          },
+          {
+            model: this.model.associations.location.target,
+            as: 'location',
+            attributes: ["name", "questionGroupId"],
+            include: [
+              {
+                model: this.model.associations.location.target?.associations?.department.target,
+                as: 'department',
+                attributes: ["name"],
+              },
+              {
+                model: this.model.associations.location.target?.associations?.questionGroup.target,
+                as: 'questionGroup',
+                attributes: ["images", "description"],
+                include: [
+                  {
+                    model: this.model.associations.location.target?.associations?.questionGroup.target?.associations?.questions.target,
+                    as: 'questions',
+                    attributes: ["content"],
 
+                  }]
+
+              }
+            ],
+          },
+          {
+            model: this.model.associations.shift.target,
+            as: 'shift',
+            attributes: ["name"],
+
+          }, {
+            model: this.model.associations.finding.target,
+            as: 'finding',
+            attributes: ["images", "reason"],
+
+          }, {
+            model: this.model.associations.correctiveAction.target,
+            as: 'correctiveAction',
+            attributes: ["images", "message"],
+
+          }
+        ],
+      });
+
+      if (item) {
+        res.status(200).json({
+          status: 200,
+          message: `${this.model.name}s fetched successfully`,
+          result: item,
+        });
+      }
+    } catch (error) {
+      res.status(500).json({
+        status: 500,
+        message: error.message,
+        result: {},
+      });
+    }
+  }
 }
 
 module.exports = new AURedTagAuditsController();
